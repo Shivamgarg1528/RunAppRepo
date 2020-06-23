467a8ce6df518422cb7687837492c22ba025653b
Shivam Garg <shivam.garg@gamepind.com>
2020-06-23 16:36:55 +0530

    added google map for tracking

--- app/src/main/java/com/st/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/st/runningapp/ui/fragments/RunFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.st.runningapp.R
-import com.st.runningapp.ui.vm.MainViewModel
 import kotlinx.android.synthetic.main.fragment_run.*
 
 class RunFragment : Fragment(R.layout.fragment_run) {
@@ -16,7 +14,7 @@ class RunFragment : Fragment(R.layout.fragment_run) {
         super.onViewCreated(view, savedInstanceState)
 
         fab.setOnClickListener {
-            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+            findNavController().navigate(RunFragmentDirections.actionRunFragmentToTrackingFragment())
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(this) {
--- app/src/main/java/com/st/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/st/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,54 @@
 package com.st.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.st.runningapp.R
-import com.st.runningapp.ui.vm.MainViewModel
+import com.st.runningapp.ui.MainActivity
+import kotlinx.android.synthetic.main.fragment_tracking.*
+import java.util.*
 
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+class TrackingFragment : Fragment() {
 
-    private val viewModel: MainViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_tracking, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainViewModel = MainActivity.getMainViewModel(this)
+
+        if (mainViewModel.isUserDataAvailable()) {
+            label.text =
+                "Let's go, " + mainViewModel.getName().toUpperCase(Locale.getDefault()) + " !"
+            label.visibility = View.VISIBLE
+        }
+
+        run.setOnClickListener {
+
+        }
+        stop.setOnClickListener {
+
+        }
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(callback)
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
