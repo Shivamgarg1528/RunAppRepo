90f2d3d4fbf4ca348dbf0869a044989e933e5849
Shivam Garg <shivam.garg@gamepind.com>
2020-06-23 16:58:27 +0530

    added location permission

--- app/src/main/java/com/st/runningapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/st/runningapp/ui/fragments/RunFragment.kt
@@ -1,24 +1,47 @@
 package com.st.runningapp.ui.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.st.runningapp.R
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.EasyPermissions
 
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         fab.setOnClickListener {
-            findNavController().navigate(RunFragmentDirections.actionRunFragmentToTrackingFragment())
+            checkPermission()
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(this) {
             requireActivity().finish()
         }
+    }
+
+    private fun checkPermission(){
+        if (EasyPermissions.hasPermissions(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            findNavController().navigate(RunFragmentDirections.actionRunFragmentToTrackingFragment())
+        } else {
+            EasyPermissions.requestPermissions(this, "We need this to track your running for giving you best results !!", 102, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        Toast.makeText(requireContext(), "We can't do anything without permission", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        checkPermission()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
